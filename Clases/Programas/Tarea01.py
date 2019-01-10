5ed8bf916ece25f1f41c9d8314b38a3695b34ad2
316060064 <saturaderketzer@gmail.com>
2019-01-10 17:04:03 -0600

    Realicé mi primer tarea del problema que busqué en la red.

---
+++ Clases/Programas/Tarea01.py
@@ -0,0 +1,13 @@
+"""Un corredor avanza 3 km en un tiempo de 10 min.
+Calcula su rapidez, es decir el valor de su velocidad en Km/h
+"""
+d = 3
+t = 10
+
+tu = t * 1.0/60
+
+V = d/tu
+
+""" print tu (me gusatria restringir a solo 3 decimales)"""
+
+print V
