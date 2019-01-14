36f3a7f943ab906eff3ead1afe9eb7f9e2ce0c75
316060064 <saturaderketzer@gmail.com>
2019-01-14 17:07:46 -0600

    Actualice el archivo Funcion01.py

--- Clases/Programas/Funcion01.py
+++ Clases/Programas/Funcion01.py
@@ -4,37 +4,13 @@ def vAbsoluto(x):
     else:
         return(-x)
 
-def coordenadas(x,y):
-    if x<5 and y<10:
-        return(3)
-    
-    if 25>x==5 and y<10:
-        return(7)
-    
-    if x>25 and y<10:
-        return(3)
+
+x=10
+while x>0:
+    x=x-1
 
 
-    if x<5 and 30>y==10:
-        return(5)
-    
-    if 25>x==5 and 30==y>=10:
-        return(10)
-    
-    if x>25 and 30>y==10:
-        return(5)
-
-
-    if x<5 and 30>y:
-        return(3)
-    
-    if 25>x==5 and 30<y:
-        return(7)
-    
-    if x>25 and 30<y:
-        return(3)
-    if x>25 and 30==y:
-        return(3)
+print x
     
     
     
