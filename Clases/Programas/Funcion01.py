17ae51d9f4e053ec5cd49179a0f7d4824a21c289
316060064 <saturaderketzer@gmail.com>
2019-01-15 14:03:30 -0600

    Realice el ejercico de raiz1 dentro de Funcion01.py

--- Clases/Programas/Funcion01.py
+++ Clases/Programas/Funcion01.py
@@ -5,12 +5,35 @@ def vAbsoluto(x):
         return(-x)
 
 
-x=10
-while x>0:
-    x=x-1
+def raiz(x):
+    h=x
+    b=1.0
+    e=0.0001
+    while vAbsoluto(b-h)>e:
+        h = (b+h)/2
+        b = x/h
+    return(h)
 
 
-print x
-    
-    
+def raiz1(x):
+    h=x
+    b=1.0
+    e=0.0001
+    C=x/x
+    while vAbsoluto(b-h)>e:
+        h = (b+h)/2
+        b = x/h
+        c = c + c
+    return(h)
+    return(c)
+
+
+
+print raiz(1)
+print raiz(4)
+print raiz(9)
+print raiz(9.1)
+print raiz(1000000)
+
+
     
