edb1d65e29d41fa2c3615892b2fafb4dd19ea84f
316060064 <saturaderketzer@gmail.com>
2019-01-17 17:11:06 -0600

    Realizamos el problema1, y el ejercicio3 en LaTeRealizamos el problema1, y el ejercicio3 en LaTeX

---
+++ Clases/Programas/Problema1S.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+from Problema1 import mcd
+
+a = input("Dame el entero mayor: ")
+b = input("Dame el entero menor: ")
+
+print "El m.c.d de %d y %d es: %d" % (a,b,mcd(a,b))
