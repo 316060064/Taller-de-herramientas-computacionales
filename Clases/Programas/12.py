86f5df6078f2e6192251db48b51c048c38fd6624
316060064 <saturaderketzer@gmail.com>
2019-01-22 16:54:14 -0600

    Realizamos el ejercicio4 de LaTeX y el 12 de python

---
+++ Clases/Programas/12.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+
+
+'''
+alumnos = []
+>>> alumnos.append([9,8,10,9])
+>>> alumnos([9])
+ alumnos.append([9])
+>>> alumnos.append([6,9,10,8,9,10,10,9])   '''
+
+alumnos =[]
+
+'''
+alumnos.append([9,8,10,9])
+alumnos.append([9])
+alumnos.append([6,9,10,8,9,10,10,9])'''
+
+
+
+'''
+for i in range(len(alumnos)):
+    for j in range(len(alumnos[i])):
+            calificacion = alumnos [i][j]
+            print '%4d' % calificacion,
+    print
+'''
+
+'''for alumno in alumnos:
+    for calificacion in alumno:
+            print '%8d' % calificacion,
+        
+    print'''
+
+
+alumnos = []
+cali= []
+sem = []
+mat = []
+cali2= []
+sem2 = []
+mat2 = []
+cali3= []
+sem3 = []
+mat3 = []
+
+ex = [5,8,10,9,3]
+tarea = [8,9,2,6,4]
+asis = [15,16,10,9,5]
+exf = [7,5,6,7,9]
+
+
+ex2 = [2,7,1,9,3]
+tarea2 = [5,6,2,3,4]
+asis2 = [10,6,4,9,5]
+exf2 = [3,4,2,7,9]
+
+ex3 = [2,8,10,9,9]
+tarea3 = [6,9,2,8,4]
+asis3 = [9,9,10,8,5]
+exf3 = [9,4,1,8,6]
+
+
+
+mat.append(ex)
+mat.append(tarea)
+mat.append(asis)
+mat.append(exf)
+
+sem.append(mat)
+cali.append(sem)
+alumnos.append(cali)
+
+
+mat2.append(ex2)
+mat2.append(tarea2)
+mat2.append(asis2)
+mat2.append(exf2)
+
+sem2.append(mat2)
+cali2.append(sem2)
+alumnos.append(cali2)
+
+mat3.append(ex3)
+mat3.append(tarea3)
+mat3.append(asis3)
+mat3.append(exf3)
+
+sem3.append(mat3)
+cali3.append(sem3)
+alumnos.append(cali3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+        
+        
+    
--- Clases/Programas/tabla.py
+++ Clases/Programas/tabla.py
@@ -1,11 +1,19 @@
 
 gradosC = [-10,-5,0,5,10]
 gradosF = [-10*9.0/5 +32, -5*9.0/5+32, 0*9.0/5+32, 5*9.0/5+32, 10*9.0/5+32]
-list(zip(gradosC,gradosF))
+'''list(zip(gradosC,gradosF))
 for c, f in zip(gradosC,gradosF):
-    print ("{} grados centigrados: {} grados Farenheit".format(c,f))
+    print ("{} grados centigrados: {} grados Farenheit".format(c,f))'''
 
 
+
+
+table = [[C,F] for C,F in zip(gradosC,gradosF)]
+
+print table
+from pprint import pprint
+
+pprint(table)
 
     
 
