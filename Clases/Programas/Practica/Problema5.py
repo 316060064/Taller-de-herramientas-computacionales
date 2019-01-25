375193ef7a526372ed8c404d6812e638050c51f2
316060064 <saturaderketzer@gmail.com>
2019-01-25 16:51:47 -0600

    Inicie con una idea el problema 5

--- Clases/Programas/Practica/Problema5.py
+++ Clases/Programas/Practica/Problema5.py
@@ -1,6 +1,34 @@
-def malla(x,y):
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+'''
+Josue Artemio Hernandez Rodriguez, 316060064
+Lo que hice fue usar while, y poner como
+condicion que mientras n sea mayor a 1 se repita el pro-
+ceso hasta que no sea 1
+'''
+'''def malla(a,b,c=1):
+    for i in range(a,b,c):
+        c *=.5
+        lista = []
+        lista.append(i)
+        print lista
+        '''
+        
     
 
+n = input ("¿Cuantos valores?: ")
+L = []
+for i in range(n):
+    valor = input("Dame el valor")
+    L.append(valor)
+    
+m = input ("¿Cuantos valores?: ")
+L2 = []
+for i in range(m):
+    valor = input("Dame el valor")
+    L2.append(valor)
 
 
-    x**2/25 - y**2/49
+  
+
+    
